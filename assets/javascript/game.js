dec2e36478cf7e6dcce4b35730f935d444906c0d
Easter Bohanon <eastdabeast@email.arizona.edu>
2018-12-01 00:12:31 -0700

    yo Imma take a break

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -61,20 +61,64 @@ function updatePage(didPlayerWin) {
     $("#wins").empty();
 
     if (didPlayerWin === true) {
-       wins++;
+      
         $("#wins").htm('<p> Wins: </p>' + wins);
         alert("You Won Yo! You may be the next Einstein!");
         resetGame();
         showTargetScore();
     }
     else if (didPlayerWi === false) {
-        losses++;
+       
         $("#losses").htm('<p> Losses: </p>' + losses);
         alert("Yeah thats a hard no! Try Again!");
         resetGame();
         showTargetScore();
     } 
 }
+
+// Hopefully will show the total number the user is clicking
+function updateUserScore(gems) {
+userScore += gems[gems.attr("users-score")].points;
+}
+
+// will hopefully show the User's guess
+
+function pleaseShowUserGuess() {
+    var userScoreNum = $("#users-score").text(userScore);
+    $("#users-score").html();
+    $("#users-score").html(userScoreNum);
+
+}
+
+resetGame();
+updatePage();
+pleaseShowUserGuess();
+
+
+
+//hopefully this will create an onclick event for the pictures
+$(".gems").on("click", function(event) {
+    updateUserScore($(this));
+    pleaseShowUserGuess();
+
+
+    if (userScore === randomNum) {
+        wins++;
+        resetGame();
+        updatePage(true);
+
+    }
+
+    else if (userScore > randomNum) {
+        losses++;
+        resetGame();
+        updatePage(false);
+
+    }
+
+});
+
+
 
 
 
